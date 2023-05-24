b6a6b8def56a49b84947e702d5b3989fc8a37ca0
Dang Dung <dangdung239>
2023-05-24 23:59:37 +0700

    Renew dias base

--- dias-base/src/main/java/com/dungtran/dias/base/recyclerview/BaseListener.kt
+++
@@ -1,5 +0,0 @@
-package com.dungtran.dias.base.recyclerview
-
-interface BaseListener {
-
-}
--- dias-base/src/main/java/com/dungtran/dias/base/recyclerview/BaseViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.dungtran.dias.base.recyclerview
-
-import androidx.databinding.ViewDataBinding
-import androidx.recyclerview.widget.RecyclerView
-
-open class BaseViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
-
-}
